95c3385d79e77cb2e30ec6a988cfe4b75804a4cc
Gabriel Braun <braun.pineschi@gmail.com>
2022-09-14 08:04:04 -0300

    3B - Teoria

--- braunchem/main.py
+++ braunchem/main.py
@@ -8,25 +8,21 @@ from shutil import copytree
 
 
 def main():
-    arsenal = load_arsenal('database')
+    arsenal = load_arsenal("database")
     arsenal.generate_pdfs()
 
     copy_r(
-        'database/arsenal.json',
-        '/home/braun/Documents/Developer/braunchem-web/database/arsenal.json'
+        "database/arsenal.json",
+        "/home/braun/Documents/Developer/braunchem-web/database/arsenal.json",
     )
 
     copytree(
-        'database/images/',
-        '/home/braun/Documents/Developer/braunchem-web/public/',
-        dirs_exist_ok=True
+        "database/images/",
+        "/home/braun/Documents/Developer/braunchem-web/public/",
+        dirs_exist_ok=True,
     )
 
-    copytree(
-        'out/',
-        '/home/braun/Documents/Drive/Material/Listas',
-        dirs_exist_ok=True
-    )
+    copytree("out/", "/home/braun/Documents/Drive/Material/Listas", dirs_exist_ok=True)
 
 
 if __name__ == "__main__":
